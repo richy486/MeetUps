a8f282b6f5975f4ecd6f5cb1cd8079bf1547158d
richy486 <richy486@gmail.com>
2015-07-01 20:03:30 +0200

    Initial Commit

---
+++ MeetUps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetUps
+//
+//  Created by Richard Adem on 1/07/2015.
+//  Copyright (c) 2015 Richard Adem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetUps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetUps
+//
+//  Created by Richard Adem on 1/07/2015.
+//  Copyright (c) 2015 Richard Adem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
